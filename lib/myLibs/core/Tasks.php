8d2c425aed50dc2362d2eb4c03f766e57bfbda4c
Lionel Péramo <peramo.lionel@gmail.com>
2013-10-12 22:18:20 +0200

    Console colors changes.

--- lib/myLibs/core/Tasks.php
+++ lib/myLibs/core/Tasks.php
@@ -88,8 +88,8 @@ class Tasks
     $alt = 0;
     foreach(\config\Routes::$_ as $route => $details){
       $chunks = $details['chunks'];
-      echo ($alt % 2) ? lightGray() : white(), PHP_EOL, sprintf('%-20s', $route) , '- url  : ' , $chunks[0], PHP_EOL;
-      echo sprintf('%22s', ' '), 'path : ' . $chunks[1] . '/' . $chunks[2] . '/' . $chunks[3] . 'Controller/' . $chunks[4] , PHP_EOL, PHP_EOL;
+      echo ($alt % 2) ? cyan() : lightBlue(), PHP_EOL, sprintf('%-20s', $route), 'url  : ' , $chunks[0], PHP_EOL;
+      echo sprintf('%20s', ' '), 'path : ' . $chunks[1] . '/' . $chunks[2] . '/' . $chunks[3] . 'Controller/' . $chunks[4] , PHP_EOL, PHP_EOL;
       echo endColor();
       $alt++;
     }
