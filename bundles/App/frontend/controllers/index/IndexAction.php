e4b00ae3efb2fd767910af353b92861c6029486d
Lionel Péramo <contact@lionel-peramo.com>
2019-07-26 20:20:29 +0200

    Adding missing PHPDoc about throwing exceptions. Replacing some 'self' keyword in templates by '$this' + PHPDoc in order to fix some PHPStorm warnings like "Undefined class self"! Adding a TODO on AllConfig.php. Some modifications in order to keep staying under 120 characters line limit.

--- bundles/App/frontend/controllers/index/IndexAction.php
+++ bundles/App/frontend/controllers/index/IndexAction.php
@@ -10,6 +10,9 @@ use lib\myLibs\Controller;
 
 class IndexAction extends Controller
 {
+  /**
+   * @throws \lib\myLibs\LionelException
+   */
   public function indexAction() {
     echo $this->renderView('index.phtml', []);
   }
--- config/prod/AllConfig.php
+++ config/prod/AllConfig.php
@@ -8,8 +8,10 @@ define('CACHE_TIME', 300); // 5 minutes(5*60)
 class AllConfig
 {
   public static $verbose = 0,
-    /* In order to not make new AllConfig::$blabla before calling CACHE_PATH, use directly AllConfig::$cache_path in this case
-    (if we not use AllConfig::$blabla it will not load AllConfig even if it's in the use statement so the "defines" aren't accessible ) */
+    /* In order to not make new AllConfig::$foo before calling CACHE_PATH, use directly AllConfig::$cache_path in this
+    case
+    (if we not use AllConfig::$foo it will not load AllConfig even if it's in the use statement so the "defines" aren't
+    accessible ) */
     $cache_path = CACHE_PATH,
     $version = 'v1',
     $defaultConn = '', // mandatory in order to modify it later if needed
