44358b3773860a4b3a9a192774bd9a18568a58a0
Lionel Péramo <contact@lionel-peramo.com>
2019-07-30 18:39:40 +0200

    Add a task "buildDev" to generate the typescripts, sass and php configuration files in case we just retrieved the project. The unversioned files are then generated.

--- lib/myLibs/console/Tasks.php
+++ lib/myLibs/console/Tasks.php
@@ -75,6 +75,31 @@ class Tasks
   /**
    * @param array|null $argv
    */
+  public static function buildDev(?array $argv = null)
+  {
+    require CORE_PATH . 'console/BuildDev.php';
+  }
+
+  /**
+   * @return array
+   */
+  public static function buildDevDesc() : array
+  {
+    return [
+      'Compiles the typescripts, sass and php configuration files (modulo the binary mask).',
+      [
+        'verbose' => '0 => Quite silent, 1 => Tells which file has been updated.',
+        'mask' => '1 => SCSS, 2 => TS, ..., 4 => routes, ..., 8 => PHP, 15 => ALL. Default to 15.',
+        'gcc' => 'Should we use Google Closure Compiler for javascript/typescript files ? Defaults to false.'
+      ],
+      ['optional', 'optional', 'optional'],
+      'Deployment'
+    ];
+  }
+
+  /**
+   * @param array|null $argv
+   */
   public static function cc(?array $argv = null)
   {
     require CORE_PATH . 'console/ClearCache.php';
