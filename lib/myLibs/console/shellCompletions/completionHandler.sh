0ad5df7274731cdb3fb0e268556fba2103415831
lionel <contact@lionel-peramo.com>
2019-09-08 17:06:36 +0200

    Adding bash compatibility. Scoping variables in the completion script.

--- lib/myLibs/console/shellCompletions/completionHandler.sh
+++ lib/myLibs/console/shellCompletions/completionHandler.sh
@@ -1,16 +1,46 @@
 #!/usr/bin/env bash
-# shellcheck source=./src/lib.sh
-. "${CORE_DIR}"/console/shellCompletions/shellCompletions.sh
+autocomplete()
+{
+  if [[ -n ${BASH+x} ]]; then
+    # shellcheck source=./shellCompletions.sh
+    . ${CURRENT_DIR}/shellCompletions.sh
 
-if [ -n "${ZSH_VERSION+x}" ]; then
-  COMP_WORDS=(${words})
-  COMP_CWORD=$((CURRENT - 1))
-fi
+    typeset SUGGESTIONS="";
+  else
+    # shellcheck source=./shellCompletions.sh
+    . "${CORE_DIR}"/console/shellCompletions/shellCompletions.sh
+  fi
 
-if [[ "${COMP_WORDS[@]:1:1}" > 1 ]]; then
-  for (( index=0; index < ${#OTRA_COMMANDS_DESCRIPTIONS[@]}; index+=1 )); do
-    compadd -S "" -X "$(echo -e "${OTRA_COMMANDS_DESCRIPTIONS[@]:((index)):1}")" "${OTRA_COMMANDS[@]:((index)):1}"
-  done
-else
-  compadd -S "" ${OTRA_COMMANDS}
-fi
+  if [ -n "${ZSH_VERSION+x}" ]; then
+    COMP_WORDS=(${words})
+    COMP_CWORD=$((CURRENT - 1))
+  fi
+
+  if [[ "${COMP_WORDS[@]:1:1}" > 1 ]]; then
+    for (( index=0; index < ${#OTRA_COMMANDS_DESCRIPTIONS[@]}; index+=1 )); do
+      typeset SUGGESTION="${OTRA_COMMANDS[@]:((index)):1}"
+
+      if [[ -n ${BASH+x} ]]; then
+        SUGGESTIONS+=" ${SUGGESTION}"
+      else
+        compadd -S "" -X "$(echo -e "${OTRA_COMMANDS_DESCRIPTIONS[@]:((index)):1}")" "${SUGGESTION}"
+      fi
+    done
+  else
+    for (( index=0; index < ${#OTRA_COMMANDS_DESCRIPTIONS[@]}; index+=1 )); do
+      typeset SUGGESTION="$(echo -e ${OTRA_COMMANDS[@]:((index)):1})"
+
+      if [[ -n ${BASH+x} ]]; then
+        SUGGESTIONS+=" ${SUGGESTION}"
+      else
+        compadd -S "" ${SUGGESTION}
+      fi
+    done
+  fi
+
+  if [ -n "${BASH+x}" ]; then
+    complete -W "${SUGGESTIONS}" otra
+  fi
+}
+
+autocomplete
