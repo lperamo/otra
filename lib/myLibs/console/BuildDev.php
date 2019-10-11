4cfc25a9349d62607139f88b0bda0b8233e0da0e
Lionel Péramo <contact@lionel-peramo.com>
2019-10-11 10:27:39 +0200

    When we add/modify ts files, we create the related 'js' folder it does not exist. Does the same for sass/scss/css files.

--- lib/myLibs/console/BuildDev.php
+++ lib/myLibs/console/BuildDev.php
@@ -179,8 +179,14 @@ foreach($iterator as $entry)
       {
         // The Google Closure Compiler application cannot overwrite a file so we have to create a temporary one
         // and remove the dummy file ...
-        $generatedTemporaryJsFile = $resourceFolder . '/js/' . $baseName . '_viaTypescript.js';
-        $generatedJsFile = $resourceFolder . '/js/' . $baseName . '.js';
+        $jsFolder = $resourceFolder . '/js/';
+
+        // if the js folder corresponding to the ts folder does not exist yet, we create it
+        if (file_exists($jsFolder) === false)
+          mkdir($jsFolder);
+
+        $generatedTemporaryJsFile = $jsFolder . '/js/' . $baseName . '_viaTypescript.js';
+        $generatedJsFile = $jsFolder . '/js/' . $baseName . '.js';
 
         // Creating a temporary typescript json configuration file suited for the OTRA watcher.
         // We need to recreate it each time because the user can alter his original configuration file
@@ -257,7 +263,14 @@ foreach($iterator as $entry)
       $generatedCssFile = $baseName . '.css';
 
       // SASS / SCSS (Implemented for Dart SASS as Ruby SASS is deprecated, not tested with LibSass)
-      $cssPath = realPath($resourceFolder . '/../css') . '/' . $generatedCssFile;
+      $cssFolder = $resourceFolder . '/css';
+
+      // if the css folder corresponding to the sass/scss folder does not exist yet, we create it
+      if (file_exists($cssFolder) === false)
+        mkdir($cssFolder);
+
+      $cssPath = realPath($cssFolder) . '/' . $generatedCssFile;
+
       list(, $return) = cli('sass ' . $resourceName . ':' . $cssPath);
 
       echo strtoupper($extension) . ' file ', returnLegiblePath($resourceName) . ' have generated ',
