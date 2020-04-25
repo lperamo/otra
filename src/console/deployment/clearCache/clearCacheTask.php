ed06fee371e4596082a0e90f66e5a65378be0962
Lionel Péramo <contact@lionel-peramo.com>
2020-04-25 23:28:30 +0200

    Adding lot of options to the 'clearCache' task to be able to remove all the cache files.

--- src/console/deployment/clearCache/clearCacheTask.php
+++ src/console/deployment/clearCache/clearCacheTask.php
@@ -2,53 +2,237 @@
 
 use config\AllConfig;
 
-$route = $argv[2] ?? null;
+define('CLEAR_CACHE_ARG_MASK', 2);
+define('CLEAR_CACHE_ARG_ROUTE', 3);
 
-// If we have chosen a specific route
-if (isset($route) === true)
+define('OTRA_SUCCESS', CLI_GREEN . '  ✔  ' . END_COLOR . PHP_EOL);
+
+define('CLEAR_CACHE_MASK_PHP_INTERNAL_CACHE', 1);
+define('CLEAR_CACHE_MASK_PHP_BOOTSTRAPS', 2);
+define('CLEAR_CACHE_MASK_CSS', 4);
+define('CLEAR_CACHE_MASK_JS', 8);
+define('CLEAR_CACHE_MASK_ROUTE_MANAGEMENT', 16);
+define('CLEAR_CACHE_MASK_CLASS_MAPPING', 32);
+define('CLEAR_CACHE_MASK_METADATA', 64);
+define('CLEAR_CACHE_MASK_DEBUGGING', 128);
+
+$mask = (int) $argv[CLEAR_CACHE_ARG_MASK] ?? 255;
+$route = $argv[CLEAR_CACHE_ARG_ROUTE] ?? null;
+
+// Handling route
+if ($mask & CLEAR_CACHE_MASK_PHP_BOOTSTRAPS
+  || $mask & CLEAR_CACHE_MASK_CSS
+  || $mask & CLEAR_CACHE_MASK_JS)
 {
   $routes = \config\Routes::$_;
 
-  // Is this an existing route ? If not ...
-  if (isset($routes[$route]) === false)
+  /**
+   * @param string $cachePath
+   * @param string $cacheRelativePath
+   * @param string $extension
+   */
+  $removeCachedFiles = function (string $cachePath, string $cacheRelativePath, string $extension) use($routes): void
   {
-    require CONSOLE_PATH . 'tools.php';
-    list($newRoute) = guessWords($route, array_keys($routes));
+    checkFolder($cachePath, $cacheRelativePath);
 
-    if ($newRoute === null)
+    if (isset($route))
+      unlinkFile($cachePath . $route . $extension, $cacheRelativePath . $route . $extension);
+    else
     {
-      echo CLI_RED, 'The route ', CLI_YELLOW, $route, CLI_RED, ' does not exist.', END_COLOR;
+      foreach(array_keys($routes) as &$routeToSuppress)
+      {
+        if (in_array(
+          $routeToSuppress,
+          ['otra_exception', 'otra_refreshSQLLogs', 'otra_clearSQLLogs', 'otra_profiler']
+        ))
+          continue;
 
-      return null;
+        unlinkFile(
+          $cachePath . $routeToSuppress . $extension,
+          $cacheRelativePath . $routeToSuppress . $extension
+        );
+      }
     }
+  };
 
-    // Otherwise, we suggest the closest name that we have found.
-    $choice = promptUser('There is no route named ' . CLI_WHITE . $route . CLI_YELLOW. ' ! Do you mean ' . CLI_WHITE .
-      $newRoute
-      . CLI_YELLOW . ' ? (y/n)');
-
-    if ('n' === $choice)
+  // If we have chosen a specific route
+  if (isset($route))
+  {
+    // Is this an existing route ? If not ...
+    if (!isset($routes[$route]))
     {
-      echo CLI_RED, 'Sorry then !', END_COLOR, PHP_EOL;
-      return null;
-    }
+      require CONSOLE_PATH . 'tools.php';
+      list($newRoute) = guessWords($route, array_keys($routes));
 
-    $route = $newRoute;
+      if ($newRoute === null)
+      {
+        echo CLI_RED, 'The route ', CLI_YELLOW, $route, CLI_RED, ' does not exist.', END_COLOR;
+
+        return null;
+      }
+
+      // Otherwise, we suggest the closest name that we have found.
+      $choice = promptUser('There is no route named ' . CLI_WHITE . $route . CLI_YELLOW. ' ! Do you mean ' . CLI_WHITE .
+        $newRoute
+        . CLI_YELLOW . ' ? (y/n)');
+
+      if ('n' === $choice)
+      {
+        echo CLI_RED, 'Sorry then !', END_COLOR, PHP_EOL;
+        return null;
+      }
+
+      $route = $newRoute;
+    }
   }
-
-  $cacheFileName = AllConfig::$cache_path . sha1('ca' . $route . VERSION . 'che');
-
-  // Is there a cache for this route ? If yes, clears it.
-  if (file_exists($cacheFileName) === true)
-    unlink($cacheFileName);
-
-  echo 'The cache for the route ' . $route . ' has been cleared.', PHP_EOL;
-
-  return null;
 }
 
-// Otherwise we clear all the other routes.
-array_map('unlink', glob(AllConfig::$cache_path . '*.cache'));
-echo 'Cache cleared.', PHP_EOL;
+/**
+ * @param string $file
+ * @param string $fileShownInTheError
+ *
+ * @throws \otra\OtraException
+ */
+function unlinkFile(string $file, string $fileShownInTheError) : void
+{
+  if (!file_exists($file))
+    return;
 
+  if (!unlink($file))
+  {
+    echo CLI_RED, 'There has been an error during removal of the file ', CLI_CYAN, $fileShownInTheError, CLI_RED, '.',
+      END_COLOR, PHP_EOL;
+    throw new \otra\OtraException('', 1, '', NULL, [], true);
+  }
+}
+
+/**
+ * @param string $folder
+ * @param string $folderShownInTheError
+ *
+ * @throws \otra\OtraException
+ */
+function checkFolder(string $folder, string $folderShownInTheError) : void
+{
+  if (!file_exists($folder))
+  {
+    echo CLI_YELLOW, 'The folder ', CLI_CYAN, $folderShownInTheError, CLI_YELLOW, ' does not exist.', END_COLOR,
+      PHP_EOL;
+    throw new \otra\OtraException('', 1, '', NULL, [], true);
+  }
+}
+
+/* **************** PHP INTERNAL CACHE **************** */
+if ($mask & CLEAR_CACHE_MASK_PHP_INTERNAL_CACHE)
+{
+  if (isset($route))
+  {
+    $cacheFileName = AllConfig::$cache_path . sha1('ca' . $route . VERSION . 'che');
+
+    // Is there a cache for this route ? If yes, clears it.
+    if (file_exists($cacheFileName) === true)
+      unlinkFile($cacheFileName, $cacheFileName);
+
+    echo 'The cache for the route ' . $route . ' has been cleared.', PHP_EOL;
+
+    return null;
+  }
+
+  // Otherwise we clear all the other routes.
+  array_map('unlink', glob(AllConfig::$cache_path . '*.cache'));
+  echo 'PHP OTRA internal cache cleared', OTRA_SUCCESS;
+}
+
+$phpCachePath = CACHE_PATH . 'php/';
+$phpRelativePath = 'cache/php';
+
+// If we want to remove route management, class mapping, metadata and debugging files, we need to check the PHP folder
+if ((
+    ($mask / CLEAR_CACHE_MASK_ROUTE_MANAGEMENT)
+    | ($mask / CLEAR_CACHE_MASK_CLASS_MAPPING)
+    | ($mask / CLEAR_CACHE_MASK_METADATA)
+    | ($mask / CLEAR_CACHE_MASK_DEBUGGING)
+  ) & 1)
+  checkFolder($phpCachePath, $phpRelativePath);
+
+/* **************** PHP BOOTSTRAPS **************** */
+if (($mask & CLEAR_CACHE_MASK_PHP_BOOTSTRAPS) >> 1)
+{
+  $removeCachedFiles($phpCachePath, $phpRelativePath, '.php');
+  echo 'PHP bootstrap(s) cleared', OTRA_SUCCESS;
+}
+
+/* **************** CSS **************** */
+if (($mask & CLEAR_CACHE_MASK_CSS) >> 2)
+{
+  $removeCachedFiles(CACHE_PATH . 'css/', 'cache/css', '.gz');
+  echo 'CSS files cleared', OTRA_SUCCESS;
+}
+
+/* **************** JS **************** */
+if (($mask & CLEAR_CACHE_MASK_JS) >> 3)
+{
+  $removeCachedFiles(CACHE_PATH . 'js/', 'cache/js', '.gz');
+  echo 'JS files cleared', OTRA_SUCCESS;
+}
+
+/* **************** ROUTE MANAGEMENT **************** */
+if (($mask & CLEAR_CACHE_MASK_ROUTE_MANAGEMENT) >> 4)
+{
+  $routeManagementFile = 'RouteManagement.php';
+  unlinkFile(
+    $phpCachePath . $routeManagementFile,
+    $phpRelativePath . $routeManagementFile
+  );
+
+  echo 'Route management file cleared', OTRA_SUCCESS;
+}
+
+/* **************** CLASS MAPPING **************** */
+if (($mask & CLEAR_CACHE_MASK_CLASS_MAPPING) >> 5)
+{
+  $classMapFile = 'ClassMap.php';
+  unlinkFile(
+    $phpCachePath . $classMapFile,
+    $phpRelativePath . $classMapFile
+  );
+
+  $prodClassMapFile = 'ProdClassMap.php';
+  unlinkFile(
+    $phpCachePath . $prodClassMapFile,
+    $phpRelativePath . $prodClassMapFile
+  );
+
+  echo 'Class mapping files cleared', OTRA_SUCCESS;
+}
+
+/* **************** CONSOLE TASKS METADATA **************** */
+if (($mask & CLEAR_CACHE_MASK_METADATA) >> 6)
+{
+  $taskClassMapFile = 'tasksClassMap.php';
+  unlinkFile(
+    $phpCachePath . $taskClassMapFile,
+    $phpRelativePath . $taskClassMapFile
+  );
+
+  $tasksHelpFile = 'tasksHelp.php';
+  unlinkFile(
+    $phpCachePath . $tasksHelpFile,
+    $phpRelativePath . $tasksHelpFile
+  );
+
+  echo 'Metadata cleared', OTRA_SUCCESS;
+}
+
+/* **************** DEBUGGING FILES **************** */
+if (($mask & CLEAR_CACHE_MASK_DEBUGGING) >> 6)
+{
+  $profilerFile = 'profiler.php';
+  unlinkFile(
+    $phpCachePath . $profilerFile,
+    $phpRelativePath . $profilerFile
+  );
+
+  echo 'Debugging files cleared', OTRA_SUCCESS;
+}
 ?>
