fcd11bf67400b2f912adbd399efb7d7a3a05c9e5
Lionel Péramo <contact@lionel-peramo.com>
2020-05-08 22:45:27 +0200

    Adding a "getPreferredLanguage" function to the translate tool file.

--- src/tools/translate.php
+++ src/tools/translate.php
@@ -6,5 +6,28 @@ if (function_exists('t') === false)
   {
     return $text;
   }
+
+  /**
+   * @param array $acceptedLanguages
+   *
+   * @return string
+   */
+  function getPreferredLanguage(array $acceptedLanguages) : string
+  {
+    function setLang(string &$lang)
+    {
+      $lang = strtok($lang, ';');
+    }
+
+    $filterLangs = function (string &$lang) use ($acceptedLanguages) : string
+    {
+      return in_array($lang, $acceptedLanguages);
+    };
+
+    $langs = explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE']);
+    array_walk($langs, 'setLang');
+
+    return array_filter($langs, $filterLangs)[0] ?: 'en';
+  }
 }
 ?>
