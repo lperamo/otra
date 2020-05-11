c05ea3f98b2b6af8798f9674a6df569b401c1b66
Lionel Péramo <contact@lionel-peramo.com>
2020-05-11 15:51:50 +0200

    Fixing return type and a bug with the callback 'setLang'.

--- src/tools/translate.php
+++ src/tools/translate.php
@@ -14,18 +14,19 @@ if (function_exists('t') === false)
    */
   function getPreferredLanguage(array $acceptedLanguages) : string
   {
-    function setLang(string &$lang)
+    // It seems to bug when I use "function setLang" instead of "$setLang = function" with the array_filter.
+    $setLang = function (string &$lang) : void
     {
       $lang = strtok($lang, ';');
-    }
+    };
 
-    $filterLangs = function (string &$lang) use ($acceptedLanguages) : string
+    $filterLangs = function (string &$lang) use ($acceptedLanguages) : bool
     {
       return in_array($lang, $acceptedLanguages);
     };
 
     $langs = explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE']);
-    array_walk($langs, 'setLang');
+    array_walk($langs, $setLang);
 
     return array_filter($langs, $filterLangs)[0] ?: 'en';
   }
