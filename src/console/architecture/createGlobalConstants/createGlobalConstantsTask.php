1a8bd9f613cea165b5b4a4df5b19e04f1046e73d
Lionel Péramo <contact@lionel-peramo.com>
2020-05-15 18:47:28 +0200

    Fixing OTRA resources paths constants.

--- src/console/architecture/createGlobalConstants/createGlobalConstantsTask.php
+++ src/console/architecture/createGlobalConstants/createGlobalConstantsTask.php
@@ -4,9 +4,10 @@ $otraProject = $vendorPosition !== false;
 $basePath = $otraProject
   ? substr(__DIR__, 0, $vendorPosition)
   : substr(__DIR__, 0, mb_strrpos(__DIR__, 'src'));
-$corePath = $basePath . ($otraProject ? 'vendor/otra/otra/src/' : 'src/');
+$otraProjectSuffix = $otraProject ? 'vendor/otra/otra/src/' : 'src/';
+$corePath = $basePath . $otraProjectSuffix;
 $consolePath = $corePath . 'console/';
-$coreResourcesPath = $corePath . 'resources/';
+$coreResourcesPath = '/' . $otraProjectSuffix . 'resources/';
 $cachePath = $basePath . 'cache/';
 $testPath = $otraProject === true
   ? $basePath . 'vendor/otra/otra/tests/'
@@ -27,11 +28,11 @@ $content = '<?php define(\'BASE_PATH\',\'' . $basePath .
   '\');define(\'OTRA_VERSION\',\'1.0.0-alpha.2.2.0' .
   '\');if(!defined(\'OTRA_PROJECT\'))define(\'OTRA_PROJECT\',' . ($otraProject ? 'true' : 'false') . ');?>';
 
-require $consolePath . 'colors.php';
+// require_once in case we do not load this file directly (console already loads colors, not composer)
+require_once $consolePath . 'colors.php';
 
 if (file_put_contents($basePath . 'config/constants.php', $content) === false)
   echo CLI_RED . 'There was a problem while writing the OTRA global constants.', END_COLOR, PHP_EOL;
 else
   echo 'OTRA global constants generated.', CLI_GREEN, ' ✔', END_COLOR, PHP_EOL;
 ?>
-
