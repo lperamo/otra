95a298ef28c619650e577f2325436ad515e239e1
Lionel Péramo <contact@lionel-peramo.com>
2020-06-15 21:03:53 +0200

    Improves maintainability of the 'hash' task. Adds tests for this task.

---
+++ tests/src/console/helpAndTools/HashTest.php
@@ -0,0 +1,56 @@
+<?php
+declare(strict_types=1);
+
+namespace src\console\helpAndTools;
+
+use otra\console\TasksManager;
+use phpunit\framework\TestCase;
+
+/**
+ * @runTestsInSeparateProcesses
+ */
+class HashTest extends TestCase
+{
+  private const
+    OTRA_TASK_HASH = 'hash',
+    OTRA_TASK_HELP = 'help',
+    BLOWFISH_SALT_LENGTH = 22;
+
+  /**
+   * @author Lionel Péramo
+   */
+  public function testHash() : void
+  {
+    // context
+
+    // testing
+    $this->expectOutputRegex('@\$2y\$03\$[a-zA-Z0-9]{' . self::BLOWFISH_SALT_LENGTH . '}@');
+
+    // launching
+    TasksManager::execute(
+      require BASE_PATH . 'cache/php/tasksClassMap.php',
+      self::OTRA_TASK_HASH,
+      ['otra.php', self::OTRA_TASK_HASH, 3]
+    );
+  }
+
+  public function testGenerateTaskMetadataHelp()
+  {
+    $this->expectOutputString(
+      CLI_WHITE .
+      str_pad(self::OTRA_TASK_HASH, TasksManager::PAD_LENGTH_FOR_TASK_TITLE_FORMATTING) .
+      CLI_LIGHT_GRAY . ': ' . CLI_CYAN .
+      'Returns a random hash.' .
+      PHP_EOL . CLI_LIGHT_CYAN .
+      '   + ' . str_pad('rounds', TasksManager::PAD_LENGTH_FOR_TASK_OPTION_FORMATTING) .
+      CLI_LIGHT_GRAY . ': ' . CLI_LIGHT_CYAN . '(' . TasksManager::OPTIONAL_PARAMETER .
+      ') ' . CLI_CYAN . 'The numbers of round for the blowfish salt. Default: 7.' . PHP_EOL . END_COLOR
+    );
+
+    TasksManager::execute(
+      require BASE_PATH . 'cache/php/tasksClassMap.php',
+      self::OTRA_TASK_HELP,
+      ['otra.php', self::OTRA_TASK_HELP, self::OTRA_TASK_HASH]
+    );
+  }
+}
