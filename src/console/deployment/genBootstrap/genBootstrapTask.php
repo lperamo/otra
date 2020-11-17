ad51da10f95ca5709257f6fb903b1ca87c694c96
Lionel Péramo <contact@lionel-peramo.com>
2020-11-17 14:51:13 +0100

    Fixes the use case where we add the 'template' key in the main configurations file while the value was false (and then useless).

--- src/console/deployment/genBootstrap/genBootstrapTask.php
+++ src/console/deployment/genBootstrap/genBootstrapTask.php
@@ -100,7 +100,7 @@ foreach(array_keys($routes) as &$route)
 
   ++$key;
 
-  if (isset($routes[$route]['resources']['template']))
+  if (isset($routes[$route]['resources']['template']) && $routes[$route]['resources']['template'] === true)
     echo CLI_WHITE, str_pad(str_pad(' ' . $route, 25, ' ', STR_PAD_RIGHT) . CLI_CYAN
         . ' [NO MICRO BOOTSTRAP => TEMPLATE GENERATED] ' . CLI_WHITE, 94, '=', STR_PAD_BOTH), END_COLOR, PHP_EOL;
   else
