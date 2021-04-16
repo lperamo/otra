29aed264574462335441b088d2f9aa928f02fe71
Lionel Péramo <contact@lionel-peramo.com>
2021-04-16 11:02:45 +0200

    New JS routing task available.
    Signed-off-by: Lionel Péramo <contact@lionel-peramo.com>

---
+++ src/console/deployment/genJsRouting/genJsRoutingTask.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * JavaScript route mapping task
+ *
+ * @author Lionel Péramo
+ * @package otra\console\deployment
+ */
+
+echo 'Generating JavaScript routing...', PHP_EOL;
+
+$routes = \config\Routes::$allRoutes;
+
+unset(
+  $routes['otra_404'],
+  $routes['otra_clearSQLLogs'],
+  $routes['otra_exception'],
+  $routes['otra_profiler'],
+  $routes['otra_refreshSQLLogs']
+);
+
+define('MAIN_RESOURCES_PATH', BASE_PATH . 'bundles/resources/');
+define('MAIN_JS_ROUTING' , MAIN_RESOURCES_PATH . 'jsRouting.js');
+define('ERASE_SEQUENCE', "\033[1A\r\033[K");
+define('OTRA_SUCCESS', CLI_GREEN . '  ✔  ' . END_COLOR);
+
+if (!file_exists(MAIN_RESOURCES_PATH))
+  mkdir(MAIN_RESOURCES_PATH);
+
+file_put_contents(
+  MAIN_JS_ROUTING,
+  'const JS_ROUTING = ' .
+  json_encode($routes, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK)
+);
+
+echo ERASE_SEQUENCE, 'JavaScript routing generated in ', CLI_LIGHT_CYAN, MAIN_JS_ROUTING, END_COLOR, OTRA_SUCCESS,
+  PHP_EOL;
+
