3001c1e0f9d14b444d92f934f8cb3aac1d60d1d3
Lionel Péramo <contact@lionel-peramo.com>
2021-05-26 15:36:49 +0200

    Finishing the main code of the 'checkConfiguration' task adding multiple conditions and their related tests and examples. Adjusts SonarQube configuration.
    Signed-off-by: Lionel Péramo <contact@lionel-peramo.com>

---
+++ tests/examples/tools/routesCheck/configurationIsNotAString.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+return [
+  'HelloWorld' => [
+    '3' => ['/helloworld', 'HelloWorld', 'frontend', 'index', 'HomeAction'],
+    'resources' => [
+      '_css' => ['pages/HelloWorld/screen'],
+      'print_css' => ['pages/HelloWorld/print'],
+      'template' => true
+    ]
+  ]
+];
