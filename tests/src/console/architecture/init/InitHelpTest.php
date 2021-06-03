f756bdf7c30acc2ecddf50ed1e1c96d75847eb5d
Lionel Péramo <contact@lionel-peramo.com>
2021-06-03 17:14:23 +0200

    Adds a test for the help of the 'init' task.
    Signed-off-by: Lionel Péramo <contact@lionel-peramo.com>

---
+++ tests/src/console/architecture/init/InitHelpTest.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+
+namespace src\console\architecture\init;
+
+use otra\console\TasksManager;
+use otra\OtraException;
+use phpunit\framework\TestCase;
+use const otra\console\{CLI_BASE, CLI_GRAY, CLI_INFO, CLI_INFO_HIGHLIGHT, END_COLOR};
+use const otra\bin\TASK_CLASS_MAP_PATH;
+
+
+/**
+ * @runTestsInSeparateProcesses
+ */
+class InitHelpTest extends TestCase
+{
+
+  private const OTRA_TASK_INIT = 'init',
+    OTRA_TASK_HELP = 'help';
+  // fixes issues like when AllConfig is not loaded while it should be
+  protected $preserveGlobalState = FALSE;
+
+  /**
+   * @author Lionel Péramo
+   * @throws OtraException
+   */
+  public function test() : void
+  {
+    $this->expectOutputString(
+      CLI_BASE .
+      str_pad(self::OTRA_TASK_INIT, TasksManager::PAD_LENGTH_FOR_TASK_TITLE_FORMATTING) .
+      CLI_GRAY . ': ' . CLI_INFO .
+      'Initializes the OTRA project.' .
+      PHP_EOL . END_COLOR
+    );
+
+    TasksManager::execute(
+      require TASK_CLASS_MAP_PATH,
+      self::OTRA_TASK_HELP,
+      ['otra.php', self::OTRA_TASK_HELP, self::OTRA_TASK_INIT]
+    );
+  }
+}
