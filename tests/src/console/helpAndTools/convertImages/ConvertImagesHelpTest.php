65feff18273f797c24478c21f1fe6004b561d23d
Lionel Péramo <contact@lionel-peramo.com>
2021-07-08 15:45:15 +0200

    Adds a new task that can convert image files from an extension to another with a quality parameter and a parameter to remove the source image or not. Adds the related automated tests.
    Signed-off-by: Lionel Péramo <contact@lionel-peramo.com>

---
+++ tests/src/console/helpAndTools/convertImages/ConvertImagesHelpTest.php
@@ -0,0 +1,55 @@
+<?php
+declare(strict_types=1);
+
+namespace src\console\helpAndTools\convertImages;
+
+use otra\console\TasksManager;
+use otra\OtraException;
+use phpunit\framework\TestCase;
+use const otra\console\{CLI_BASE, CLI_GRAY, CLI_INFO, CLI_INFO_HIGHLIGHT, END_COLOR};
+use const otra\bin\TASK_CLASS_MAP_PATH;
+
+/**
+ * @runTestsInSeparateProcesses
+ */
+class ConvertImagesHelpTest extends TestCase
+{
+  private const
+    OTRA_TASK_CONVERT_IMAGES = 'convertImages',
+    OTRA_TASK_HELP = 'help';
+
+  /**
+   * @throws OtraException
+   */
+  public function test() : void
+  {
+    // testing
+    $this->expectOutputString(
+      CLI_BASE .
+      str_pad(self::OTRA_TASK_CONVERT_IMAGES, TasksManager::PAD_LENGTH_FOR_TASK_TITLE_FORMATTING) .
+      CLI_GRAY . ': ' . CLI_INFO . 'Converts images to another format.' . PHP_EOL .
+      CLI_INFO_HIGHLIGHT .
+      '   + ' . str_pad('source', TasksManager::PAD_LENGTH_FOR_TASK_OPTION_FORMATTING) .
+      CLI_GRAY . ': ' . CLI_INFO_HIGHLIGHT . '(' . TasksManager::REQUIRED_PARAMETER .
+      ') ' . CLI_INFO . 'The source format' . PHP_EOL . CLI_INFO_HIGHLIGHT .
+      '   + ' . str_pad('destination', TasksManager::PAD_LENGTH_FOR_TASK_OPTION_FORMATTING) .
+      CLI_GRAY . ': ' . CLI_INFO_HIGHLIGHT . '(' . TasksManager::REQUIRED_PARAMETER .
+      ') ' . CLI_INFO . 'The destination format' . PHP_EOL . CLI_INFO_HIGHLIGHT .
+      '   + ' . str_pad('quality', TasksManager::PAD_LENGTH_FOR_TASK_OPTION_FORMATTING) .
+      CLI_GRAY . ': ' . CLI_INFO_HIGHLIGHT . '(' . TasksManager::OPTIONAL_PARAMETER .
+      ') ' . CLI_INFO . 'The percentage of quality. Defaults to 75.' . PHP_EOL . CLI_INFO_HIGHLIGHT .
+      '   + ' . str_pad('keep', TasksManager::PAD_LENGTH_FOR_TASK_OPTION_FORMATTING) .
+      CLI_GRAY . ': ' . CLI_INFO_HIGHLIGHT . '(' . TasksManager::OPTIONAL_PARAMETER .
+      ') ' . CLI_INFO . 'Put ' . CLI_INFO_HIGHLIGHT . 'true' . CLI_INFO .
+      ' to keep the source image as a backup. Defaults to ' . CLI_INFO_HIGHLIGHT . 'true' . CLI_INFO .'.' . PHP_EOL .
+      END_COLOR
+    );
+
+    // launching
+    TasksManager::execute(
+      require TASK_CLASS_MAP_PATH,
+      self::OTRA_TASK_HELP,
+      ['otra.php', self::OTRA_TASK_HELP, self::OTRA_TASK_CONVERT_IMAGES]
+    );
+  }
+}
---
+++ tests/src/console/helpAndTools/convertImages/ConvertTaskTest.php
@@ -0,0 +1,71 @@
+<?php
+declare(strict_types=1);
+
+namespace src\console\helpAndTools\convertImages;
+
+use otra\console\TasksManager;
+use otra\OtraException;
+use phpunit\framework\TestCase;
+
+use const otra\bin\TASK_CLASS_MAP_PATH;
+use const otra\cache\php\{BASE_PATH, TEST_PATH};
+use const otra\console\{CLI_ERROR, CLI_INFO_HIGHLIGHT};
+
+/**
+ * @runTestsInSeparateProcesses
+ */
+class ConvertTaskTest extends TestCase
+{
+  private const
+    OTRA_TASK_CONVERT_IMAGES = 'convertImages',
+    CONVERT_PATH = BASE_PATH . 'web/testsConvert/',
+    TEST_IMAGE_BASENAME = 'dummy',
+    TEST_IMAGE_EXTENSION = 'png',
+    TEST_IMAGE_DEST_EXTENSION = 'webp',
+    TEST_IMAGE_FULL_NAME = self::TEST_IMAGE_BASENAME . '.' . self::TEST_IMAGE_EXTENSION,
+    TEST_IMAGE_SOURCE_PATH = self::CONVERT_PATH . self::TEST_IMAGE_FULL_NAME,
+    TEST_IMAGE_DEST_PATH = self::CONVERT_PATH . self::TEST_IMAGE_BASENAME . '.' . self::TEST_IMAGE_DEST_EXTENSION;
+
+  /**
+   * @author Lionel Péramo
+   * @throws OtraException
+   */
+  public function test() : void
+  {
+    // context
+    mkdir(self::CONVERT_PATH);
+    copy(
+    TEST_PATH . 'examples/images/' . self::TEST_IMAGE_FULL_NAME,
+      self::TEST_IMAGE_SOURCE_PATH
+    );
+
+    // launching
+    TasksManager::execute(
+      require TASK_CLASS_MAP_PATH,
+      self::OTRA_TASK_CONVERT_IMAGES,
+      [
+        'otra.php',
+        self::OTRA_TASK_CONVERT_IMAGES,
+        self::TEST_IMAGE_EXTENSION,
+        self::TEST_IMAGE_DEST_EXTENSION,
+        '75',
+        'false'
+      ]
+    );
+
+    // testing
+    self::assertFileDoesNotExist(
+      self::TEST_IMAGE_SOURCE_PATH,
+      'Testing if ' . CLI_INFO_HIGHLIGHT . self::TEST_IMAGE_SOURCE_PATH . CLI_ERROR . ' does not exist'
+    );
+
+    self::assertFileExists(
+      self::TEST_IMAGE_DEST_PATH,
+      'Testing if ' . CLI_INFO_HIGHLIGHT . self::TEST_IMAGE_DEST_PATH . CLI_ERROR . ' exists'
+    );
+
+    // cleaning
+    unlink(self::TEST_IMAGE_DEST_PATH);
+    rmdir(self::CONVERT_PATH);
+  }
+}
