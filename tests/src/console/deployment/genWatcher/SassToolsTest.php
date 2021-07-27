4fddea4aa4951b1b72e6bfd1dee3b7825b6d628f
Lionel Péramo <contact@lionel-peramo.com>
2021-07-27 11:18:43 +0200

    'genWatcher' task : - Adds new tests and fixes others.
    Signed-off-by: Lionel Péramo <contact@lionel-peramo.com>

--- tests/src/console/deployment/genWatcher/SassToolsTest.php
+++ tests/src/console/deployment/genWatcher/SassToolsTest.php
@@ -5,9 +5,12 @@ namespace src\console\deployment\genWatcher;
 
 use otra\OtraException;
 use phpunit\framework\TestCase;
-use const otra\cache\php\{BUNDLES_PATH, CONSOLE_PATH, CORE_PATH, TEST_PATH};
-use const otra\console\deployment\genWatcher\{KEY_ALL_SASS, SASS_TREE_STRING_INIT};
-use function otra\console\deployment\genWatcher\searchSassLastLeaves;
+
+use const otra\cache\php\{BUNDLES_PATH, CACHE_PATH, CONSOLE_PATH, CORE_PATH, TEST_PATH};
+use const otra\console\{CLI_ERROR, CLI_INFO_HIGHLIGHT};
+use const otra\console\deployment\genWatcher\{KEY_ALL_SASS, KEY_FULL_TREE, SASS_TREE_CACHE_PATH, SASS_TREE_STRING_INIT};
+
+use function otra\console\deployment\genWatcher\{savingSassTree, searchSassLastLeaves, updateSassTreeAfterEvent};
 use function otra\tools\debug\dump;
 
 /**
@@ -25,10 +28,57 @@ class SassToolsTest extends TestCase
     BUNDLES_CONFIG_PATH = BUNDLES_PATH . 'config/',
     ROUTES_CONFIG_PATH = self::BUNDLES_CONFIG_PATH . 'Routes.php',
     DOT_EXTENSION = '.scss',
-    LABEL_TESTING_THE_TREE = 'Testing the sass tree... We have :';
+    LABEL_TESTING_THE_TREE = 'Testing the sass tree... We have :',
+    BIG_TREE = [
+      0 => [
+        self::SCSS_MAIN_PATH => true,
+        self::SCSS_MAIN_DEPENDENCY_LVL0_PATH => true,
+        self::SCSS_MAIN_DEPENDENCY_LVL1_PATH => true,
+        self::SCSS_MAIN_DEPENDENCY2_LVL0_2_PATH => true,
+        self::SCSS_MAIN2_PATH => true
+      ],
+      1 => [
+        1 => [0 => 0],
+        2 => [0 => 0, 1 => 4],
+        3 => [0 => 0, 1 => 4],
+      ],
+      2 => [
+        0 => [
+          1 => [
+            2 => [3 => []]
+          ],
+          3 => []
+        ],
+        4 => [
+          2 => [3 => []]
+        ],
+      ]
+    ];
 
   // fixes issues like when AllConfig is not loaded while it should be
   protected $preserveGlobalState = FALSE;
+
+  protected function setUp(): void
+  {
+    parent::setUp();
+    define('otra\\console\\deployment\\genWatcher\\SASS_TREE_CACHE_PATH',  CACHE_PATH . 'css/sassTree.php');
+    require CORE_PATH . 'tools/debug/dump.php'; // only for debugging purposes
+    require CONSOLE_PATH . 'deployment/genWatcher/sassTools.php';
+  }
+
+  protected function tearDown(): void
+  {
+    parent::tearDown();
+
+    if (file_exists(SASS_TREE_CACHE_PATH))
+      unlink(SASS_TREE_CACHE_PATH);
+
+    if (file_exists(self::ROUTES_CONFIG_PATH))
+    {
+      unlink(self::ROUTES_CONFIG_PATH);
+      rmdir(self::BUNDLES_CONFIG_PATH);
+    }
+  }
 
   private function searchSassLastLeavesContext()
   {
@@ -42,8 +92,6 @@ class SassToolsTest extends TestCase
     $argv = [];
 
     // -- including needed libraries
-    require CORE_PATH . 'tools/debug/dump.php'; // only for debugging purposes
-    require CONSOLE_PATH . 'deployment/genWatcher/sassTools.php';
     require CONSOLE_PATH . 'deployment/taskFileInit.php';
   }
 
@@ -51,7 +99,7 @@ class SassToolsTest extends TestCase
    * @author Lionel Péramo
    * @throws OtraException
    */
-  public function testSearchSassLastLeavesOneMainSass() : void
+  public function testSearchSassLastLeaves_OneMainSass() : void
   {
     // context
     // Defining variables - part 1
@@ -98,17 +146,13 @@ class SassToolsTest extends TestCase
       $sassTree,
       self::LABEL_TESTING_THE_TREE . PHP_EOL . dump($sassTree)
     );
-
-    // cleaning
-    unlink(self::ROUTES_CONFIG_PATH);
-    rmdir(self::BUNDLES_CONFIG_PATH);
   }
 
   /**
    * @author Lionel Péramo
    * @throws OtraException
    */
-  public function testSearchSassLastLeavesTwoMainSass() : void
+  public function testSearchSassLastLeaves_TwoMainSass() : void
   {
     // context
     // Defining variables - part 1
@@ -143,6 +187,66 @@ class SassToolsTest extends TestCase
 
     // testing
     self::assertEquals(
+      self::BIG_TREE,
+      $sassTree,
+      self::LABEL_TESTING_THE_TREE . PHP_EOL . dump($sassTree)
+    );
+  }
+
+  /**
+   * @throws OtraException
+   */
+  public function testSavingSassTree()
+  {
+    // context
+    // If there is already a cache for the sass tree, we remove it to be sure it will not interfere with our test
+    if (file_exists(SASS_TREE_CACHE_PATH))
+      unlink(SASS_TREE_CACHE_PATH);
+
+    define(__NAMESPACE__ . '\\CACHED_SASS_TREE', TEST_PATH . 'examples/deployment/sassTree.php');
+
+    // launching
+    savingSassTree(self::BIG_TREE);
+
+    // testing
+    static::assertFileExists(SASS_TREE_CACHE_PATH);
+    static::assertFileEquals(
+      SASS_TREE_CACHE_PATH,
+      CACHED_SASS_TREE,
+    'Testing ' . CLI_INFO_HIGHLIGHT . SASS_TREE_CACHE_PATH . CLI_ERROR . ' against ' . CLI_INFO_HIGHLIGHT .
+    CACHED_SASS_TREE . CLI_ERROR . ';'
+    );
+  }
+
+  /**
+   * Tests when a dependency (so a file beginning by '_' is updated.
+   *
+   * @throws OtraException
+   */
+  public function testUpdateSassTreeAfterEvent()
+  {
+    // context
+    $this->searchSassLastLeavesContext();
+    $sassTree = self::BIG_TREE;
+    $sassTreeKeys = array_keys($sassTree[KEY_ALL_SASS]);
+
+    // launching
+    foreach ($sassTree[KEY_FULL_TREE] as $importingFile => &$importedFiles)
+    {
+      updateSassTreeAfterEvent(
+        $sassTree,
+        $sassTreeKeys,
+        'scss',
+        0, // array_search(self::SCSS_MAIN_PATH, $sassTreeKeys)
+        $importingFile,
+        $importedFiles,
+        1,
+        [1] // [array_search(self::SCSS_MAIN_DEPENDENCY_LVL0_PATH, $sassTreeKeys)]
+      );
+    }
+
+    // testing
+    self::assertEquals(
       [
         0 => [
           self::SCSS_MAIN_PATH => true,
@@ -154,14 +258,13 @@ class SassToolsTest extends TestCase
         1 => [
           1 => [0 => 0],
           2 => [0 => 0, 1 => 4],
-          3 => [0 => 0, 1 => 4],
+          3 => [1 => 4],
         ],
         2 => [
           0 => [
             1 => [
               2 => [3 => []]
-            ],
-            3 => []
+            ]
           ],
           4 => [
             2 => [3 => []]
@@ -169,11 +272,7 @@ class SassToolsTest extends TestCase
         ]
       ],
       $sassTree,
-      self::LABEL_TESTING_THE_TREE . PHP_EOL . dump($sassTree)
+      'Testing if $sassTree has correctly been udpated...'
     );
-
-    // cleaning
-    unlink(self::ROUTES_CONFIG_PATH);
-    rmdir(self::BUNDLES_CONFIG_PATH);
   }
 }
