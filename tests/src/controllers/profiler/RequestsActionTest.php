2c0ba65a3ed75ef4d793321b5834f145ca9c108f
Lionel Péramo <contact@lionel-peramo.com>
2021-12-06 11:58:34 +0100

    Profiler and session :   - Adds the session id in the profiler urls   - The session files are now stored with a hashed session id, not a hashed route name   - Automated tests updated accordingly
    Signed-off-by: Lionel Péramo <contact@lionel-peramo.com>

--- tests/src/controllers/profiler/RequestsActionTest.php
+++ tests/src/controllers/profiler/RequestsActionTest.php
@@ -7,9 +7,12 @@ use otra\console\TasksManager;
 use otra\controllers\profiler\RequestsAction;
 use otra\OtraException;
 use phpunit\framework\TestCase;
+use function otra\console\convertLongArrayToShort;
 use const otra\bin\TASK_CLASS_MAP_PATH;
-use const otra\cache\php\{APP_ENV, BASE_PATH, BUNDLES_PATH, CORE_PATH, DEV, OTRA_PROJECT};
+use const otra\cache\php\
+{APP_ENV, BASE_PATH, BUNDLES_PATH, CACHE_PATH, CORE_PATH, DEV, OTRA_PROJECT, TEST_PATH};
 use function otra\tools\delTree;
+use const otra\config\VERSION;
 
 /**
  * @runTestsInSeparateProcesses
@@ -65,10 +68,34 @@ class RequestsActionTest extends TestCase
   public function test() : void
   {
     // context
+    require TEST_PATH . 'config/AllConfigGood.php';
     require CORE_PATH . 'templating/blocks.php';
     $_GET['route'] = 'HelloWorld';
+    $_GET['session_id'] = session_id();
     $_SERVER['HTTP_HOST'] = 'https://dev.otra-framework.tech';
     file_put_contents(BASE_PATH . 'logs/' . DEV . '/trace.txt', '');
+    $responseHeaders = [];
+    $headers = headers_list();
+
+    foreach ($headers as $header)
+    {
+      $header = explode(':', $header);
+      $responseHeaders[array_shift($header)] = trim(implode(':', $header));
+    }
+
+    // session id equals '' so no need to include it in the file name
+    file_put_contents(
+      CACHE_PATH . 'php/sessions/' . sha1('ca' . VERSION . 'che') . '.php',
+      '<?php declare(strict_types=1);namespace otra\cache\php\sessions;return ' .
+      convertLongArrayToShort([
+        'GET' => $_GET,
+        'POST' => $_POST,
+        'COOKIE' => $_COOKIE,
+        'SESSION' => [],
+        'requestHeaders' => [],
+        'responseHeaders' => $responseHeaders,
+      ]) . ';' . PHP_EOL
+    );
 
     // launching
     ob_start();
