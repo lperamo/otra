b1a70e2d8ed91dd9c422f9ccba81b1543cc4ab9e
Lionel Péramo <contact@lionel-peramo.com>
2021-12-09 18:15:30 +0100

    loadStaticRoute.php :  - Fixes the fatal error when no security configuration files have been customized
    Signed-off-by: Lionel Péramo <contact@lionel-peramo.com>

--- src/init/web/loadStaticRoute.php
+++ src/init/web/loadStaticRoute.php
@@ -14,7 +14,11 @@ use function otra\services\{addCspHeader,addPermissionsPoliciesHeader};
 class MasterController {public static array $nonces = [];}
 require BASE_PATH . 'config/AllConfig.php';
 require CORE_PATH . 'services/securityService.php';
-define(__NAMESPACE__ . '\\SECURITY_ROUTE_PATH', CACHE_PATH . 'php/security/' . $_SERVER[APP_ENV] . DIR_SEPARATOR . OTRA_ROUTE . '.php');
+define(
+  __NAMESPACE__ . '\\TMP_SECURITY_ROUTE_PATH',
+  CACHE_PATH . 'php/security/' . $_SERVER[APP_ENV] . DIR_SEPARATOR . OTRA_ROUTE . '.php'
+);
+define(__NAMESPACE__ . '\\SECURITY_ROUTE_PATH', file_exists(TMP_SECURITY_ROUTE_PATH) ? TMP_SECURITY_ROUTE_PATH : null);
 header('Content-Encoding: gzip');
 
 addCspHeader(OTRA_ROUTE, SECURITY_ROUTE_PATH);
