80eb005c1cbfcd0e0de197da5592db526cedd54e
Lionel Péramo <contact@lionel-peramo.com>
2022-08-04 11:39:12 +0200

    - Symfony YAML updated from 5.2.10 to 6.1.3
    Signed-off-by: Lionel Péramo <contact@lionel-peramo.com>

--- tests/src/console/deployment/genClassMap/GenClassMapTaskTest.php
+++ tests/src/console/deployment/genClassMap/GenClassMapTaskTest.php
@@ -39,7 +39,7 @@ class GenClassMapTaskTest extends TestCase
 
     // testing
     $content = '';
-    define(__NAMESPACE__ . '\\OTRA_MAX_FOLDERS', 291);
+    define(__NAMESPACE__ . '\\OTRA_MAX_FOLDERS', 304);
 
     for ($currentFolder = 1; $currentFolder < OTRA_MAX_FOLDERS; ++$currentFolder)
     {
