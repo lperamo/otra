ec4b766e6b6637535365263f4b2ba7dee6f06457
Lionel Péramo <peramo.lionel@gmail.com>
2023-10-22 17:24:25 +0200

    Updates the OTRA version in all the files that contains it. Adds a message in the first initialization of OTRA and remove the `buildDev` step.
    Signed-off-by: Lionel Péramo <peramo.lionel@gmail.com>

--- src/console/composerInit.php
+++ src/console/composerInit.php
@@ -4,7 +4,7 @@ namespace src\console;
 
 use Exception;
 use const otra\cache\php\CACHE_PATH;
-use const otra\console\{CLI_BASE, CLI_ERROR, END_COLOR};
+use const otra\console\{CLI_BASE, CLI_ERROR, CLI_INFO_HIGHLIGHT, END_COLOR};
 use function otra\console\architecture\createGlobalConstants\createGlobalConstants;
 use function otra\console\architecture\init\init;
 use function otra\console\deployment\buildDev\buildDev;
@@ -31,7 +31,9 @@ try
   version();
   init();
   echo CLI_BASE, 'Building assets...', END_COLOR, PHP_EOL;
-  buildDev(['bin/otra.php', 'buildDev', '0', '7', 'true', '1']);
+  echo 'Configure the path in the variable ', CLI_INFO_HIGHLIGHT, '$nodeBinariesPath', CLI_BASE, ' in ',
+    CLI_INFO_HIGHLIGHT, 'config/dev/AllConfig.php', CLI_BASE, ' and ', CLI_INFO_HIGHLIGHT, 'config/dev/AllConfig.php',
+    CLI_BASE, '.', PHP_EOL, 'Then launch ', CLI_INFO_HIGHLIGHT, 'otra buildDev 0 7 true 1', CLI_BASE, '.', PHP_EOL;
 } catch (Exception $exception)
 {
   echo CLI_ERROR, $exception->getTraceAsString(), END_COLOR, PHP_EOL;
